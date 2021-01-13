b61ed678b6d1b43a4e785b55a4143a96b2bcedd8
Isabel Ferrara <ifm95@hotmail.com>
2021-01-13 17:20:00 -0600

    changes to fit safaru i

--- src/Components/home/Header.js
+++ src/Components/home/Header.js
@@ -33,15 +33,12 @@ const Header = () => {
         width:'20em',
         height:'17em',
         display:'flex',
-        padding:'0px 25px 0px 25px',
+        padding:'20px 25px 20px 25px',
         alignItems:'center',
-        justifyContent:'center',
+        justifyContent:'space-between',
         flexDirection:'column',
-        backgroundColor:'gray',
         margin:'10px',
-        position:'relative', 
-        bottom:'20px',
-        background: 'rgba( 255, 255, 255, 0.60 )',
+        background: 'rgba( 255, 255, 255, 0.50 )',
         boxShadow: '0 8px 32px 0 rgba( 31, 38, 135, 0.37 )',
         backdropFilter: 'blur( 9.5px )',
         WebkitBackdropFilter: 'blur( 9.5px )',
@@ -49,31 +46,17 @@ const Header = () => {
         zIndex:'1'
       }
 
-      const buttonHead={
-        position:'absolute',
-        top:'35%', 
-        left:'15%', 
-        fontSize:'20px', 
-        color:'white', 
-        height:'60px', 
-        width:'300px', 
-        backgroundColor:'#EF325A',
-        hover: {
-          backgroundColor:'red',
-        }
-      }
-
 
     return (
         <div>
             <div style={header}>
-            <div>
-            
-            <h1 style={{position:'absolute',top:'5%', left:'13%', fontSize:'100px', color:'black', fontFamily:'sans-serif'}}> Servibev </h1>
-            <h2 style={{position:'absolute',top:'19%', left:'12%', fontSize:'30px', color:'black'}}>Expertos en jarabes para bebidas</h2>
-            <PlayCircleOutlined onClick={showModal} style={{fontSize:'100px', color:'black', opacity:'0.4'}}   />           
-            </div>
-        <div style={{ display:'flex', flexDirection:'row', position:'absolute', marginTop:'800px'}}>
+              <div>
+              
+              <h1 style={{position:'absolute',top:'5%', left:'13%', fontSize:'100px', color:'black', fontFamily:'sans-serif'}}> Servibev </h1>
+              <h2 style={{position:'absolute',top:'19%', left:'12%', fontSize:'30px', color:'black'}}>Expertos en jarabes para bebidas</h2>
+              <PlayCircleOutlined onClick={showModal} style={{ position:'absolute', left:'0', right:'0',fontSize:'100px', color:'black', opacity:'0.4', textAlign:'center', padding:'auto'}}   />           
+              </div>
+        <div style={{ display:'flex', flexDirection:'row', marginTop:'800px'}}>
             <div style={threeCardsHeader}>
                 <h2 style={{fontSize:'2em',fontWeight:'400'}}>Servicio</h2>
                 <p style={{lineHeight:'17px'}}>Adipisicing reprehenderit non pariatur anim Lorem culpa in. Non fugiat amet proident qui ut ex incididunt et nostrud voluptate nisi in pariatur. Pariatur fugiat occaecat tempor.</p>
